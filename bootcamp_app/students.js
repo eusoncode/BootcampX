feec3b7ccfd70659dbb93f234f15fcb07e662cf3
eusoncode <git@github.com:eusoncode/Kata.git>
2023-09-01 23:25:33 -0600

    SQL Injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,14 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`SELECT students.id AS student_id, students.name AS student_name, cohorts.name AS cohort 
+// Store query as string
+const queryString = `SELECT students.id AS student_id, students.name AS student_name, cohorts.name AS cohort 
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5}`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+WHERE cohorts.name LIKE $1
+LIMIT $2;`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit]; // Store all potentially malicious values in an array.
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
   })
-})
-.catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
